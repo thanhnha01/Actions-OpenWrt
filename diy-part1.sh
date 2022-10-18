8ff5772bd62c5f2f239f9cdf7b48202b955582c0
thanhnha01 <96094930+thanhnha01@users.noreply.github.com>
2022-10-18 18:09:26 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo "src-git modemfeed https://github.com/koshev-msk/modemfeed.git" >> feeds.conf.default
